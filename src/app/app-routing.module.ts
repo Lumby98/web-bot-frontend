973744e365c4b7fd2bb608010a0890686cf93cba
Lumby98 <56871098+Lumby98@users.noreply.github.com>
2021-09-02 09:44:02 +0200

    Revert "failed routing"
    This reverts commit 37ba63a54686df03d075f4ae428c9862c219a797.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
-import {ScraperComponent} from "./scraper/scraper.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'scraper', component: ScraperComponent},
+  {path: 'scraper', loadChildren: () => import('./scraper/scraper.module').then(m => m.ScraperModule)},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: 'home'}];
