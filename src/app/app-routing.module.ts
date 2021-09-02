37ba63a54686df03d075f4ae428c9862c219a797
Lumby98 <56871098+Lumby98@users.noreply.github.com>
2021-09-02 09:43:45 +0200

    failed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ScraperComponent} from "./scraper/scraper.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'scraper', loadChildren: () => import('./scraper/scraper.module').then(m => m.ScraperModule)},
+  {path: 'scraper', component: ScraperComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: 'home'}];
