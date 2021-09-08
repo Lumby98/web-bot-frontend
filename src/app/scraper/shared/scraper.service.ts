2269312864e9a6329bb87c5de5ed1f2bc45eff62
Lumby98 <56871098+Lumby98@users.noreply.github.com>
2021-09-08 19:29:21 +0200

    removed unused import

--- src/app/scraper/shared/scraper.service.ts
+++ src/app/scraper/shared/scraper.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ProductDTO} from "../../shared/dto/product.dto";
 import {environment} from "../../../environments/environment";
