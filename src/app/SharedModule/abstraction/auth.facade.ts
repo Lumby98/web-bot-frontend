0982d29fe32fc71eef9b4c2627ead84043f44d56
zanaxdk <31621017+zanaxdk@users.noreply.github.com>
2021-11-12 12:03:41 +0100

    Update auth.facade.ts

--- src/app/SharedModule/abstraction/auth.facade.ts
+++ src/app/SharedModule/abstraction/auth.facade.ts
@@ -1,8 +1,14 @@
 import {Injectable} from "@angular/core";
 import {AuthService} from "../core/services/auth.service";
+import {UserDto} from "../dto/user.dto";
 
 @Injectable()
 export class AuthFacade{
   constructor(private auth: AuthService,) {
   }
+
+  getLocalUser() : UserDto | null{
+    return this.auth.GetCurrentUserFromLocalStorage()
+  }
+
 }
