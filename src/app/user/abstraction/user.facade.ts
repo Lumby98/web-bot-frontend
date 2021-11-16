817a7298a29874c88c4531608112e25fdc72bcce
scupak <anto1095@easv365.dk>
2021-11-16 13:06:02 +0100

    Update user.facade.ts

--- src/app/user/abstraction/user.facade.ts
+++ src/app/user/abstraction/user.facade.ts
@@ -4,10 +4,11 @@ import {Store} from "@ngxs/store";
 import {Router} from "@angular/router";
 import {UserService} from "../core/services/user.service";
 import {RegisterDto} from "../core/models/register.dto";
-import {ClearUserError, UpdateUserError} from "../core/state/users.actions";
+import {ClearUserError, InsertOrUpdateUser, UpdateUserError} from "../core/state/users.actions";
 import {AuthState} from "../../SharedModule/core/state/auth/auth.state";
 import {ClearError} from "../../SharedModule/core/state/auth/auth.actions";
 import {UserState} from "../core/state/users.state";
+import {take} from "rxjs/operators";
 
 @Injectable()
 export class UserFacade {
@@ -37,5 +38,15 @@ export class UserFacade {
     this.store.dispatch(new UpdateUserError(error));
   }
 
+  getUserByIdFromApi(id: number){
+    this.userService.userById(id).pipe(take(1)).subscribe(u => {
+      if (u) {
+        this.store.dispatch(new InsertOrUpdateUser(u));
+      } else {
+        throw new Error('failed to find user')
+      }
+  })
 
+
+}
 }
