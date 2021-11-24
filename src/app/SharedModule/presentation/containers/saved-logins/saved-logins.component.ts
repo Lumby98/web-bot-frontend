4054dcac94972395e652173b3fec8f7c273c8bc4
scupak <anto1095@easv365.dk>
2021-11-24 13:21:52 +0100

    saved logins component part 1.

--- src/app/SharedModule/presentation/containers/saved-logins/saved-logins.component.ts
+++ src/app/SharedModule/presentation/containers/saved-logins/saved-logins.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthFacade} from "../../../abstraction/auth.facade";
+import {LoginTypeEnum, LoginTypeMapping} from "../../../core/enums/loginType.enum";
+import {MatSelectChange} from "@angular/material/select";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-saved-logins',
@@ -9,14 +12,77 @@ import {AuthFacade} from "../../../abstraction/auth.facade";
   styleUrls: ['./saved-logins.component.scss']
 })
 export class SavedLoginsComponent implements OnInit {
-
+  hide: Boolean;
+  loginInsertForm: FormGroup;
+  keyVerifyFrom: FormGroup;
   currentKey$: Observable<string>
+  public selectedType: any;
+  public loginTypes: { value: LoginTypeEnum; type: string; }[] | undefined;
+  error$!: Observable<any>;
+
+  get username() {
+    return this.loginInsertForm.get('username');
+  }
+
+  get password() {
+    return this.loginInsertForm.get('password');
+  }
+
+  get key() {
+    return this.keyVerifyFrom.get('key');
+  }
+
+  private  get  selectedLoginType():LoginTypeEnum {
+    return this.selectedType ? this.selectedType.value: null;
+  }
 
   constructor(private formBuilder: FormBuilder, private authFacade: AuthFacade) {
+    this.error$ = this.authFacade.getErrorObservable();
+    this.hide = true;
     this.currentKey$ = this.authFacade.getCurrentKey();
+    this.loginInsertForm = this.formBuilder.group({
+      loginType: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.keyVerifyFrom = this.formBuilder.group({
+      key: ["" , Validators.required]
+    });
   }
 
   ngOnInit(): void {
+    this.loginInsertForm.controls.username.disable();
+    this.loginInsertForm.controls.password.disable();
+
+    this.loginTypes = LoginTypeMapping;
   }
 
+  /**
+   * gets the selected value of the dropdown menu
+   * @param event
+   */
+  selectChangeHandler(event: MatSelectChange) {
+    this.selectedType = event.value;
+    this.loginInsertForm.controls.username.enable();
+    this.loginInsertForm.controls.password.enable();
+  }
+
+  insertLogin() {
+
+  }
+
+  verifyKey(){
+    this.authFacade.verify({password: this.key?.value}).pipe(take(1)).subscribe( err => {
+      this.authFacade.updateError(err);
+    });
+
+  }
+
+  clearError() {
+    //this.error = undefined;
+    this.authFacade.clearError();
+  }
 }
+
+
