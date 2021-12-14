ef3a078030b6ecb470c350621abccf4150e545a2
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-12-14 14:55:45 +0100

    test

--- src/app/SharedModule/core/enums/loginType.enum.ts
+++ src/app/SharedModule/core/enums/loginType.enum.ts
@@ -1,9 +1,11 @@
 export enum LoginTypeEnum {
   ORTOWEAR,
   NESKRID,
+  HULTAFORS,
 }
 
 export const LoginTypeMapping = [
   { value: LoginTypeEnum.ORTOWEAR, type: 'Ortowear' },
   { value: LoginTypeEnum.NESKRID, type:  'Neskrid' },
+  { value: LoginTypeEnum.HULTAFORS, type:  'Hultafors' },
 ];
